4a11027abe880652131b0d4557a205426eac40ac
Sebastien <srenault.contact@gmail.com>
2018-06-13 09:22:14 +0200

    Export several accounts

--- tools/transactions.py
+++ tools/transactions.py
@@ -1,37 +1,39 @@
 import os
 import argparse
 import time
-from datetime import date
+from datetime import date, datetime
 from cm.config import Config
 from cm.client import Client
 
 def main():
     parser = argparse.ArgumentParser(description='Export CM transactions.')
-    parser.add_argument('account', help='Account id to export')
+    parser.add_argument('accountids', help='Account ids to export', nargs='+')
     args = parser.parse_args()
 
-    directory = ensure_export_dir()
-    start_date = last_export_date(directory)
-    export(directory, args.account, start_date)
+    for account_id in args.accountids:
+        directory = ensure_export_dir(account_id)
+        start_date = last_export_date(directory)
+        export(directory, account_id, start_date)
 
-def ensure_export_dir() -> str:
-    directory = Config.Transactions.directory
+def ensure_export_dir(account_id: str) -> str:
+    directory = Config.Transactions.directory + '/' + account_id
     if not os.path.exists(directory):
         os.makedirs(directory)
     return directory
 
-def last_export_date(directory: str) -> date:
+def last_export_date(directory: str) -> object:
     files = [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
-    timestamps = sorted(list(map(lambda file: int(file), files)), reverse=True)
-    return date.fromtimestamp(timestamps[0]) if len(timestamps) > 0 else None
+    filtered_files = list(filter(lambda file: os.path.splitext(file)[1] == '.ofx', files))
+    date_format = Config.Transactions.date_format
+    dates = sorted(map(lambda file: datetime.strptime(os.path.splitext(file)[0], date_format), filtered_files), reverse=True)
+    return dates[0] if len(dates) > 0 else None
 
-def export(directory: str, account_id: str, start_date: date) -> None:
+def export(directory: str, account_id: str, start_date: object) -> None:
     with Client(Config.Login.username, Config.Login.password) as cm:
         cm.authenticate()
-        ofx_file_name = str(int(time.time()))
-        ofx_file_path = directory + '/' + ofx_file_name
-        date_format = Config.Transactions.date_format
-        start = start_date.strftime(date_format) if start_date else None
+        ofx_file_name = datetime.utcnow().strftime(Config.Transactions.date_format)
+        ofx_file_path = f"{directory}/{ofx_file_name}.ofx"
+        start = start_date.strftime('%Y/%m/%d') if start_date else None
         cm.download_ofx_to(ofx_file_path, account_id, start)
 
 if __name__ == '__main__':
